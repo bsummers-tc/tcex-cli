7485f0308be33d88ce5c8ad6667157fbcd9f5277
Bracey Summers <bsummers@threatconnect.com>
2023-03-30 20:07:10 -0500

    APP-3926 - switched from setup.py/setup.cfg to pyproject using setuptools APP-3926 - restored missing files APP-3926 - cleaned up .gitignore APP-3926 - update packages in README.md

--- tcex_cli/__metadata__.py
+++ tcex_cli/__metadata__.py
@@ -1,9 +1,3 @@
 """TcEx Framework metadata."""
-__author__ = 'ThreatConnect (support@threatconnect.com)'
-__author_email__ = 'support@threatconnect.com'
-__description__ = 'ThreatConnect Exchange App CLI Framework'
-__license__ = 'Apache License, Version 2'
-__package_name__ = 'tcex_cli'
-__url__ = 'https://github.com/ThreatConnect-Inc/tcex-cli'
+__license__ = 'Apache-2.0'
 __version__ = '0.0.1'
-__download_url__ = f'https://github.com/ThreatConnect-Inc/tcex-cli/tarball/{__version__}'
