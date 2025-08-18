114f273d8fe3ac9c58f5bcb633de985a466ed9bd
Bracey Summers <bsummers@threatconnect.com>
2025-08-18 14:55:06 -0500

    - restructured app_input_cli.py to be more maintainable

--- tcex_cli/cli/app_input/app_input_cli.py
+++ tcex_cli/cli/app_input/app_input_cli.py
@@ -3,7 +3,9 @@
 # standard library
 import json
 import logging
+from functools import cached_property
 from pathlib import Path
+from typing import Any
 
 # first-party
 from tcex_cli.app.config.install_json import InstallJson
@@ -25,13 +27,100 @@ class AppInputCli(CliABC):
         self.include_optional = include_optional
         self.ij = InstallJson()
 
-    @staticmethod
-    def get_sample_values(param: ParamsModel, playbook_datatype: str):
-        """Get sample values for the variable."""
-        sample_data = None
+        # properties
+        self.inputs = {}
+        self.kvstore = {}
 
-        # Mapping playbookDataType to sample values
-        playbook_datatype_map = {
+    def add_input(self, name: str, value: str):
+        """."""
+        self.inputs[name] = value
+        Render.panel.info(f'[green]Added parameter: {name} with value: {value}[/green]')
+
+    def add_kvstore(self, variable: str, value: Any):
+        """."""
+        self.kvstore[variable] = value
+        Render.panel.info(f'[green]Added kvstore variable: {variable} with value: {value}[/green]')
+
+    def get_param_default(self, param: ParamsModel) -> Any:
+        """Get the default value for the parameter."""
+        try:
+            return json.loads(param.default)
+        except (json.JSONDecodeError, TypeError):
+            panel_msg = f'Error parsing input for {param.name} -> {param.default}'
+            Render.panel.failure(panel_msg)
+
+    def get_playbook_datatype(self, param: ParamsModel) -> str:
+        """Get the playbook data type for the parameter."""
+        if param.type == 'KeyValueList':
+            return 'KeyValueArray'
+
+        playbook_data_types = self.get_playbook_datatypes(param)
+        match len(playbook_data_types):
+            case 0:
+                return 'String'
+
+            case 1:
+                return playbook_data_types[0]
+
+            case _:
+                return self.prompt_user_for_playbook_datatype(playbook_data_types)
+
+    def get_playbook_datatypes(self, param: ParamsModel) -> list[str]:
+        """Get the playbook data types for the parameter."""
+        if 'Any' in param.playbook_data_type:
+            if len(param.playbook_data_type) > 1:
+                Render.panel.warning(
+                    f'Parameter {param.name} has multiple playbookDataTypes, but "Any" is '
+                    'included. Using "Any" should not be combined with other types.'
+                )
+            return [
+                'Binary',
+                'BinaryArray',
+                'KeyValue',
+                'KeyValueArray',
+                'String',
+                'StringArray',
+                'TCEntity',
+                'TCEntityArray',
+            ]
+
+        return param.playbook_data_type or []
+
+    def get_stage_value(self, param: ParamsModel, playbook_datatype: str, variable: str):
+        """Get the value for the parameter based on its type."""
+        example_value = self.playbook_datatype_map.get(playbook_datatype)
+        if example_value is None:
+            panel_msg = f'Unsupported playbookDataType for {param.name}: {playbook_datatype}'
+            Render.panel.failure(panel_msg)
+
+        match param.type:
+            case 'EditChoice':
+                if param.default:
+                    self.add_kvstore(variable, param.default)
+                else:
+                    self.add_kvstore(variable, '|'.join(param.valid_values))
+
+            case 'KeyValueList':
+                if param.default:
+                    self.add_kvstore(variable, self.get_param_default(param))
+                else:
+                    self.add_kvstore(variable, example_value)
+
+            case 'String':
+                if param.default:
+                    self.add_kvstore(variable, param.default)
+                else:
+                    self.add_kvstore(variable, example_value)
+
+    @staticmethod
+    def get_variable(param: ParamsModel, playbook_datatype: str) -> str:
+        """Get the variable name for the parameter."""
+        return f'#App:1022:{param.name}!{playbook_datatype}'
+
+    @cached_property
+    def playbook_datatype_map(self):
+        """Map of playbook data types to sample values."""
+        return {
             'Any': 'sampleString',
             'Binary': '<base64 encoded string>',
             'BinaryArray': ['<base64 encoded string>'],
@@ -43,43 +132,15 @@ class AppInputCli(CliABC):
             'TCEntityArray': [{'id': '123', 'type': 'Address', 'value': '1.1.1.1'}],
         }
 
-        # handle default value for String type
-        if param.default is not None and playbook_datatype == 'String':
-            sample_data = param.default
-        elif param.type == 'Boolean':
-            # handle Boolean type
-            sample_data = '|'.join(['true', 'false'])
-        elif param.type == 'KeyValueList':
-            # handle KeyValueList type
-            if playbook_datatype in playbook_datatype_map:
-                value = playbook_datatype_map[playbook_datatype]
-                sample_data = [{'key': 'sampleKey', 'value': value}]
-            else:
-                panel_msg = f'Unsupported playbookDataType for {param.name}: {playbook_datatype}'
-                Render.panel.failure(panel_msg)
-        elif playbook_datatype == 'String':
-            # handle String type with validValues
-            sample_data = '|'.join(param.valid_values) if param.valid_values else 'sampleString'
-        elif playbook_datatype in playbook_datatype_map:
-            sample_data = playbook_datatype_map[playbook_datatype]
-        else:
-            panel_msg = f'Unsupported playbookDataType for {param.name}: {playbook_datatype}'
-            Render.panel.failure(panel_msg)
-
-        return sample_data
-
-    def prompt_user_for_playbook_datatype(self, param: ParamsModel) -> str:
+    def prompt_user_for_playbook_datatype(self, playbook_data_types: list[str]) -> str:
         """Prompt user for playbookDataType."""
-        datatype_choice = Render.prompt.ask(
+        return Render.prompt.ask(
             'This input has multiple playbookDataTypes, please choose one: ',
-            choices=param.playbook_data_type,
-            default=param.playbook_data_type[0],
+            choices=playbook_data_types,
+            default=playbook_data_types[0],
             show_choices=True,
             show_default=True,
         )
-        if datatype_choice not in param.playbook_data_type:
-            Render.panel.failure(f'Invalid choice: {datatype_choice}. Skipping parameter.')
-        return datatype_choice
 
     def prompt_user_for_optional_params(self, param: ParamsModel) -> bool:
         """Prompt user for whether to include optional parameters."""
@@ -96,15 +157,11 @@ class AppInputCli(CliABC):
 
     def generate_app_inputs(self):
         """Generate the app_inputs.json file."""
-        kvstore = {}
-        inputs = {}
-
-        app_id = '1022'  # static value per instructions
         for param in self.ij.model.params:
             Render.panel.info(f'[blue]Processing parameter: {param.name}[/blue]')
 
             # Handle boolean with a true default value, this makes it required
-            if param.type == 'Boolean' and str(param.default).lower() == 'true':
+            if param.type == 'Boolean':
                 param.required = True
 
             # Handle optional parameters
@@ -114,62 +171,38 @@ class AppInputCli(CliABC):
                     _logger.info(f'Skipping optional parameter: {param.name}')
                     continue
 
-            # Handle playbookDataType with 'Any'
-            if 'Any' in param.playbook_data_type:
-                if len(param.playbook_data_type) > 1:
-                    Render.panel.warning(
-                        f'Parameter {param.name} has multiple playbookDataTypes, but "Any" is '
-                        'included. Using "Any" should not be combined with other types.'
-                    )
-                param.playbook_data_type = [
-                    'Binary',
-                    'BinaryArray',
-                    'KeyValue',
-                    'KeyValueArray',
-                    'String',
-                    'StringArray',
-                    'TCEntity',
-                    'TCEntityArray',
-                ]
+            match param.type:
+                # Boolean values cannot be staged
+                case 'Boolean':
+                    if param.default is not None:
+                        self.add_input(param.name, str(param.default).lower())
+                    else:
+                        self.add_input(param.name, 'false|true')
 
-            # Handle Choice and MultiChoice types, these params don't take p
-            if param.type in ('Choice', 'MultiChoice'):
-                param.playbook_data_type = ['String']
+                case 'Choice' | 'MultiChoice':
+                    if param.default:
+                        self.add_input(param.name, param.default)
+                    else:
+                        self.add_input(param.name, '|'.join(param.valid_values))
 
-            # Handle parameters with no playbookDataType
-            if not param.playbook_data_type:
-                if param.default:
-                    value = param.default
-                elif param.valid_values:
-                    value = '|'.join(param.valid_values)
-                else:
-                    value = 'sampleString'
-                inputs[param.name] = value
+                case 'EditChoice' | 'KeyValueList' | 'String':
+                    # get the appropriate playbook data type
+                    playbook_data_type = self.get_playbook_datatype(param)
 
-                Render.panel.info(
-                    f'[green]Added parameter: {param.name} with value: {value}[/green]'
-                )
-            else:
-                # Handle parameters with multiple playbookDataTypes
-                if len(param.playbook_data_type) > 1:
-                    playbook_datatype = self.prompt_user_for_playbook_datatype(param)
-                else:
-                    playbook_datatype = param.playbook_data_type[0]
+                    # create a variable to stage
+                    variable = self.get_variable(param, playbook_data_type)
 
-                variable = f'#App:{app_id}:{param.name}!{playbook_datatype}'
-                # special handling for KeyValueList
-                if param.type == 'KeyValueList':
-                    variable = f'#App:{app_id}:{param.name}!KeyValueArray'
+                    # always add the input with the value as the variable
+                    self.add_input(param.name, variable)
 
-                sample_values = self.get_sample_values(param, playbook_datatype)
-                kvstore[variable] = sample_values
-                inputs[param.name] = variable
-                Render.panel.info(
-                    f'[green]Added parameter: {param.name} with variable: {variable}[/green]'
-                )
+                    # get the value and add the staged data
+                    self.get_stage_value(param, playbook_data_type, variable=variable)
 
-        output = {'stage': {'kvstore': kvstore}, 'inputs': inputs}
+        output = {'inputs': self.inputs, 'stage': {'kvstore': self.kvstore}}
+        self.write_output_file(output)
 
+    def write_output_file(self, output: dict):
+        """Write the output to a JSON file."""
         # Write to a single file
         output_file = Path('app_inputs.json')
         if output_file.exists():
@@ -184,6 +217,5 @@ class AppInputCli(CliABC):
                 Render.panel.failure('Aborted. File not overwritten.')
             output_file.unlink()
 
-        # create the app_inputs.json file
         with output_file.open('w') as f:
             json.dump(output, f, indent=4)
